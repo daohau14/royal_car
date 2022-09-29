4ede047d7780da5ece40bb2928fa74a6d07483e5
daohau14 <daohau140200@gmail.com>
2022-09-29 14:00:27 +0700

    upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,30 @@ function carousel() {
   x[slideIndex-1].style.display = "block";
   // setTimeout(carousel, 3000); // Change image every 2 seconds
 }
+
+// nav bar responsive
+function togge_menu() {
+  var togge_header = document.querySelector(".header-mobile-menu");
+    if (togge_header.style.display === "block") {
+      togge_header.style.display = "none";
+    } else {
+      togge_header.style.display = "block";
+    }
+}
+
+function togge_car() {
+  var togge_car = document.querySelector(".subnav-car");
+    if (togge_car.style.display === "block") {
+      togge_car.style.display = "none";
+    } else {
+      togge_car.style.display = "block";
+    }
+}
+function togge_page() {
+  var togge_car = document.querySelector(".subnav-page");
+    if (togge_car.style.display === "block") {
+      togge_car.style.display = "none";
+    } else {
+      togge_car.style.display = "block";
+    }
+}
